1689270b6fbcb604423a846fbe42474f2e6ccb4a
NagendraVarma1 <nagendravarmal512@gmail.com>
2024-06-14 13:14:07 +0530

    small changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,34 @@
-import {  Nav,  Navbar } from "react-bootstrap";
-import classes from './NavBar.module.css'
+import { Nav, Navbar } from "react-bootstrap";
+import classes from "./NavBar.module.css";
 
 const NavBar = () => {
-    return (
-        <Navbar className={classes.navbar} >
-            <Navbar.Brand className={classes.brand}><h1>Portfolio</h1></Navbar.Brand>
-            <Nav className={classes.navlinkbox}>
-                <Nav.Link className={classes.links}>Home</Nav.Link>
-                <Nav.Link className={classes.links}>Qualification</Nav.Link>
-                <Nav.Link className={classes.links}>Projects</Nav.Link>
-                <Nav.Link className={classes.links}>Contact</Nav.Link>
-            </Nav>
-            
-        </Navbar>
-    )
-}
+  return (
+    <div className={classes.navbar}>
+      <h1 className={classes.brand}>Portfolio</h1>
+
+      <div className={classes.burgerBtn}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="35"
+          height="35"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          className={classes.logo}
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
+        </svg>
+      </div>
+      <ul className={classes.navlinkbox}>
+        <li className={classes.links}>Home</li>
+        <li className={classes.links}>Qualification</li>
+        <li className={classes.links}>Projects</li>
+        <li className={classes.links}>Contact</li>
+      </ul>
+    </div>
+  );
+};
 
 export default NavBar;
