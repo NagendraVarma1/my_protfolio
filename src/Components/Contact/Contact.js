f930bbb6d184a954513ab08ec0d0d1e51251e175
NagendraVarma1 <nagendravarmal512@gmail.com>
2024-06-17 21:59:21 +0530

    Contact added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,48 @@
+import { useRef } from "react";
 import classes from "./Contact.module.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_4nxta9u", "template_3dc75mj", form.current, {
+        publicKey: "xQMjD94juk6RfmzYB",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          e.target.reset();
+          alert('Mail Sent...Thank you!')
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div id="contact" className={classes.contactBg}>
       <h1 className={classes.header}>CONTACT</h1>
+      <form className={classes.form} ref={form} onSubmit={sendEmail}>
+        <div className={classes.inputDiv}>
+          <label>Name</label>
+          <input type="text" name="user_name" required/>
+        </div>
+        <div className={classes.inputDiv}>
+          <label>Email</label>
+          <input type="email" name="user_email" required/>
+        </div>
+        <div className={classes.inputDiv}>
+          <label>Message</label>
+          <textarea name="message" required/>
+        </div>
+        <div className={classes.btn}>
+          <input type="submit" value="Send" />
+        </div>
+      </form>
     </div>
   );
 };
